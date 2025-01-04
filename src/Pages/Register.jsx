7e79a41ddf1c0c91b6222aedf452b9645ea0c5ff
Amr maher <amrm80233@>
2025-01-04 20:22:24 +0200

    feat: Update Register component with user information handling
    - Added state variables for first name, second name, email, phone number, and password
    - Implemented form submission handler to update user information
    - Enhanced Register form UI with updated input fields and button styles
    - Added checkbox for agreeing to terms and conditions
    - Updated styling for input fields and buttons to align with the design

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,110 +1,141 @@
+import { useEffect, useRef, useState } from "react";
 import RegisterImage from "../Assets/Register.png";
 import "../Style/Register.css";
 
 function Register() {
+    const [FirstName, setFirstName] = useState("")
+    const [SecondName, setSecondName] = useState("");
+    const [Email, setEmail] = useState("")
+    const [PhoneNumber, setPhoneNumber] = useState("");
+    const [Password, setPassword] = useState("");
+    const [UserInfo,setUserInfo] = useState("")
+    const handelForm = (event) => {
+        event.preventDefault()
+    }
+    
+    const HandelRegister = () => {
+        setUserInfo({ name: `${FirstName} ${SecondName}`, email: Email, phone: PhoneNumber, password: Password });
+        
+    }
   return (
     <>
       <section>
         <div className="container">
           <div className="row">
-            {/* صورة التسجيل */}
             <div className="col-xl-6 col-12 d-flex justify-content-center align-items-center mb-4">
               <div className="Register-image text-center">
                 <img src={RegisterImage} alt="" className="img-fluid" />
               </div>
             </div>
-
-            {/* نموذج التسجيل */}
             <div className="col-xl-6 col-12">
               <div className="RegisterForm p-3">
-                {/* العنوان */}
-                <div className="row">
-                  <div className="form-title text-end">
-                    <h3>تسجيل جديد</h3>
-                    <p>
-                      دعنا نساعدك جميعًا حتى تتمكن من الوصول إلى حسابك الشخصي.
-                    </p>
+                <form
+                  onSubmit={(event) => {
+                    handelForm(event);
+                  }}
+                >
+                  <div className="row">
+                    <div className="form-title text-end">
+                      <h3>تسجيل جديد</h3>
+                      <p>
+                        دعنا نساعدك جميعًا حتى تتمكن من الوصول إلى حسابك الشخصي.
+                      </p>
+                    </div>
                   </div>
-                </div>
 
-                {/* الاسم الأول واسم العائلة */}
-                <div className="row mb-3">
-                  <div className="col-xl-6 col-12 mb-3 mb-xl-0">
-                    <input
-                      type="text"
-                      className="form-control firstNameInput"
-                      placeholder="أدخل الاسم الأول"
-                      aria-label="First name"
-                    />
+                  <div className="row mb-3">
+                    <div className="col-xl-6 col-12 mb-3 mb-xl-0">
+                      <input
+                        type="text"
+                        className="form-control firstNameInput"
+                        placeholder="أدخل الاسم الأول"
+                        aria-label="First name"
+                        onChange={(e) => {
+                          setFirstName(e.target.value);
+                        }}
+                      />
+                    </div>
+                    <div className="col-xl-6 col-12">
+                      <input
+                        type="text"
+                        className="form-control secondNameInput"
+                        placeholder="أدخل اسم العائلة"
+                        aria-label="Last name"
+                        onChange={(e) => {
+                          setSecondName(e.target.value);
+                        }}
+                      />
+                    </div>
                   </div>
-                  <div className="col-xl-6 col-12">
-                    <input
-                      type="text"
-                      className="form-control secondNameInput"
-                      placeholder="أدخل اسم العائلة"
-                      aria-label="Last name"
-                    />
-                  </div>
-                </div>
 
-                {/* البريد الإلكتروني */}
-                <div className="row mb-3">
-                  <div className="col-12">
-                    <input
-                      type="email"
-                      className="form-control inputEmail"
-                      placeholder="أدخل بريدك الإلكتروني"
-                    />
+                  <div className="row mb-3">
+                    <div className="col-12">
+                      <input
+                        type="email"
+                        className="form-control inputEmail"
+                        placeholder="أدخل بريدك الإلكتروني"
+                        onChange={(e) => {
+                          setEmail(e.target.value);
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
 
-                {/* رقم الهاتف */}
-                <div className="row mb-3">
-                  <div className="col-12">
-                    <input
-                      type="number"
-                      className="form-control phoneNumber"
-                      placeholder="أدخل رقم الهاتف الخاص بك"
-                    />
+                  <div className="row mb-3">
+                    <div className="col-12">
+                      <input
+                        type="number"
+                        className="form-control phoneNumber"
+                        placeholder="أدخل رقم الهاتف الخاص بك"
+                        onChange={(e) => {
+                          setPhoneNumber(e.target.value);
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
 
-                {/* كلمة المرور */}
-                <div className="row mb-3">
-                  <div className="col-12">
-                    <input
-                      type="password"
-                      className="form-control inputPassword"
-                      placeholder="أدخل كلمة المرور الخاصة بك"
-                    />
+                  <div className="row mb-3">
+                    <div className="col-12">
+                      <input
+                        type="password"
+                        className="form-control inputPassword"
+                        placeholder="أدخل كلمة المرور الخاصة بك"
+                        onChange={(e) => {
+                          setPassword(e.target.value);
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
 
-                {/* زر التسجيل */}
-                <div className="row mb-3">
-                  <div className="col-12">
-                    <button className=" w-100 Register-button">
-                      تسجيل جديد
-                    </button>
+                  <div className="row mb-3">
+                    <div className="col-12">
+                      <button
+                        className=" w-100 Register-button"
+                        onClick={() => {
+                          HandelRegister();
+                        }}
+                      >
+                        تسجيل جديد
+                      </button>
+                    </div>
                   </div>
-                </div>
-
-                {/* شروط الخدمة */}
-                <div className="row">
-                  <div className="col-12 text-end">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="flexCheckDefault"
-                    />
-                    <label
-                      className="form-check-label"
-                      htmlFor="flexCheckDefault"
-                    >
-                      وافق على شروط الخدمة وسياسة الخصوصية الخاصة بك
-                    </label>
+                  <div className="row">
+                    <div className="col-12 text-end">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="flexCheckDefault"
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="flexCheckDefault"
+                      >
+                        وافق على <span>شروط الخدمة</span> وسياسة الخصوصية
+                        <span>الخاصة بك</span>
+                      </label>
+                    </div>
                   </div>
-                </div>
+                </form>
               </div>
             </div>
           </div>
