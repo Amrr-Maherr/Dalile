ae6ea9bfdd6ff09c784f976d3de471e9bcaa0bc4
Amr maher <amrm80233@>
2025-01-04 15:48:41 +0200

    Update React to version 18.2.0 and react-dom to ensure compatibility with react-router-dom
    - Updated React version from 19.x.x to 18.2.0
    - Updated react-dom version to match React version
    - Updated dependencies to ensure compatibility with React 18.x.x

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import "@fontsource/cairo";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
-
 import NavBar from './Components/NavBar';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,66 +1,63 @@
 import { Link } from "react-router-dom";
-import "../Style/NavBarStyle.css";
-import LogoImage from "../Assets/Logo.png";
-
+import LogoImage from "../Assets/Logo.png"
+import "../Style/NavBarStyle.css"
 function NavBar() {
   return (
-    <>
-      <nav className="navbar navbar-expand-xl bg-white shadow">
-        <div className="container">
-          <button
-            className="navbar-toggler text-white"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 d-flex gap-4 mb-lg-0">
-              <li className="nav-item">
-                <Link className="active text-dark" to="/SearchPage">
-                  <i className="fa fa-search"></i>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="active text-dark" to="/favorites">
-                  <i className="fa fa-heart"></i>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="active text-dark" to="/NotificationsPage">
-                  <i className="fa fa-bell"></i>
-                </Link>
-              </li>
-            </ul>
-            <ul className="navbar-nav me-auto mb-2 d-flex gap-4 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-a active text-dark" to="/contact">
-                  اتصل بنا
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-a active text-dark" to="/Explore">
-                  استكشف
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-a active text-dark" to="/home">
-                  الرئيسية
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <Link className="navbar-brand active logo text-dark" to="#">
-            دليل المدينة <img src={LogoImage} alt="" />
-          </Link>
-          <div className="user-image"></div>
+    <nav className="navbar navbar-expand-xl bg-white shadow">
+      <div className="container">
+        <button
+          className="navbar-toggler text-white"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 d-flex gap-4 mb-lg-0">
+            <li className="nav-item">
+              <Link className="active text-dark" >
+                <i className="fa fa-search"></i>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="active text-dark" >
+                <i className="fa fa-heart"></i>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="active text-dark" >
+                <i className="fa fa-bell"></i>
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav me-auto mb-2 d-flex gap-4 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-a active text-dark" >
+                اتصل بنا
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-a active text-dark" >
+                استكشف
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-a active text-dark" >
+                الرئيسية
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
+        <Link className="navbar-brand active logo text-dark">
+          دليل المدينة <img src={LogoImage} alt="" />
+        </Link>
+        <div className="user-image"></div>
+      </div>
+    </nav>
   );
 }
 
