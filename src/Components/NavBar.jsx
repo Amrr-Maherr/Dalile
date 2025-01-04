07daebe7c3f273b308b9df7dcec11e53536ddff3
Amr maher <amrm80233@>
2025-01-04 15:35:03 +0200

    feat: install react-router-dom and update navbar links to use Link for navigation
    - Installed `react-router-dom` to enable client-side routing.
    - Replaced all anchor tags (`a`) with `Link` components from `react-router-dom` in the navbar.
    - Updated the navigation links for Search, Favorites, Notifications, Contact, Explore, and Home pages.

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,67 +1,66 @@
-import "../Style/NavBarStyle.css"
-import LogoImage from "../Assets/Logo.png"
+import "../Style/NavBarStyle.css";
+import LogoImage from "../Assets/Logo.png";
+
 function NavBar() {
-    return (
-      <>
-        <nav className="navbar navbar-expand-xl bg-white shadow">
-          <div className="container">
-            <button
-              className="navbar-toggler text-white"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 d-flex gap-4 mb-lg-0">
-                <li className="nav-item ">
-                  <a className="active text-dark" href="/SearchPage">
-                    <i class="fa fa-search"></i>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="active text-dark" href="/favorites">
-                    <i class="fa fa-heart"></i>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="active text-dark" href="/NotificationsPage">
-                    <i class="fa fa-bell"></i>
-                  </a>
-                </li>
-              </ul>
-              <ul className="navbar-nav me-auto mb-2 d-flex gap-4 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-a active text-dark" herf="/contact">
-                    اتصل بنا
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-a active text-dark" href="/Explore">
-                    استكشف
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-a active text-dark" href="/home">
-                    الرئيسية
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <a className="navbar-brand active logo text-dark" to="#">
-              دليل المدينة <img src={LogoImage} alt="" />
-            </a>
-            <div className="user-image"></div>
+  return (
+    <>
+      <nav className="navbar navbar-expand-xl bg-white shadow">
+        <div className="container">
+          <button
+            className="navbar-toggler text-white"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 d-flex gap-4 mb-lg-0">
+              <li className="nav-item ">
+                <a className="active text-dark" href="/SearchPage">
+                  <i className="fa fa-search"></i>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="active text-dark" href="/favorites">
+                  <i className="fa fa-heart"></i>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="active text-dark" href="/NotificationsPage">
+                  <i className="fa fa-bell"></i>
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav me-auto mb-2 d-flex gap-4 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-a active text-dark" href="/contact">
+                  اتصل بنا
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-a active text-dark" href="/Explore">
+                  استكشف
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-a active text-dark" href="/home">
+                  الرئيسية
+                </a>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </>
-    );
+          <a className="navbar-brand active logo text-dark" to="#">
+            دليل المدينة <img src={LogoImage} alt="" />
+          </a>
+          <div className="user-image"></div>
+        </div>
+      </nav>
+    </>
+  );
 }
-export default NavBar
+
+export default NavBar;
