fc7683a4f205f4e4bcaffb6bb23149f57f8c750a
Amr maher <amrm80233@>
2025-01-07 12:13:39 +0200

    Add BestRestaurantsCard component with styling Created the BestRestaurantsCard component to display individual restaurant details. Includes restaurant name, image, ratings, and operational hours. Displays a brief description with a location marker. Styled the card layout and elements such as title, rating, and operational status. Added associated CSS styles to define card dimensions, layout, and visual appearance. Styled the image, body background, and text alignment. Defined custom styles for review section, map location, and open/close status.

--- src/Components/BestRestaurants.jsx
+++ src/Components/BestRestaurants.jsx
@@ -1,24 +1,62 @@
+import { useEffect } from "react";
 import "../Style/BestRestaurants.css"
+import BestRestaurantsCard from "./BestRestaurantsCard";
+import axios from "axios";
+import { useState } from "react";
+
 function BestRestaurants() {
-    return (
-      <>
-        <section>
-          <div className="container">
-            <div className="row text-end">
-              <div className="col-12 ">
-                <div className="BestRestaurants-title">
-                  <h3>افضل المطاعم</h3>
-                  <div className="BestRestaurants-hr ms-auto"></div>
-                  <p>استكشف افضل الاماكن القريبه منك</p>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-                
+  const token = "252|ojXK8TnYLRtnQX3WERkm3HPltn6RJBPq0IntDkXW54640e26";
+    const [restaurants, setRestaurants] = useState([]);
+    const [loading,setLoading] = useState(true)
+  useEffect(() => {
+    axios
+      .get("https://dalil.mlmcosmo.com/api/top-rated-places", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+          setRestaurants(response.data); // Store the response data
+          setTimeout(() => {
+              setLoading(false);
+          },5000)
+      })
+      .catch((error) => {
+          console.log(error);
+          setLoading(true)
+      });
+  }, []);
+
+  return (
+    <section>
+      <div className="container">
+        <div className="row text-end">
+          <div className="col-12">
+            <div className="BestRestaurants-title">
+              <h3>افضل المطاعم</h3>
+              <div className="BestRestaurants-hr ms-auto"></div>
+              <p>استكشف افضل الاماكن القريبه منك</p>
             </div>
           </div>
-        </section>
-      </>
-    );
+        </div>
+        <div className="row my-4">
+          {loading ? (
+            <>
+              <div className="loader"></div>
+            </>
+          ) : (
+            <>
+              {restaurants.map((restaurant) => (
+                <BestRestaurantsCard
+                  key={restaurant.id}
+                  restaurant={restaurant}
+                />
+              ))}
+            </>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 }
 export default BestRestaurants
